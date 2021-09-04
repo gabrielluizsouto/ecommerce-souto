9dfaa2bf5bad026b97a4fdff8f3d55f684b8a118
Gabriel Luiz F Souto <gabrielluizferraz@gmail.com>
2021-09-04 16:56:40 -0300

    Comment all departments link

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,12 +4,11 @@ import Link from 'next/link'
 const Menu: React.FC = () => {
     return (
         <div className={styles.menuContainer}>
-            // @ts-ignore
-            <a href="/" className={styles.menuCategory}>
+            {/*<a href="/" className={styles.menuCategory}>
                 <span>
                     All Products
                 </span>
-            </a>
+            </a>*/}
             <a href="/category/tvs" className={styles.menuCategory}>
                 <span>
                     TVs
