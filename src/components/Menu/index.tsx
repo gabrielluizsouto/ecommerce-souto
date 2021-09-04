e1971033fe25798047e9fea37acaa99d9169b2d2
Gabriel Luiz F Souto <gabrielluizferraz@gmail.com>
2021-09-04 16:42:42 -0300

    Update to avoid build problems

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,13 @@
 import styles from './Menu.module.css'
+import Link from 'next/link'
 
 const Menu: React.FC = () => {
     return (
         <div className={styles.menuContainer}>
-            <a href="/" className={styles.menuCategory}>
-                <span>
+            <Link href="/" className={styles.menuCategory}>
+                <a><span>
                     All Products
-                </span>
+                </span></a>
             </a>
             <a href="/category/tvs" className={styles.menuCategory}>
                 <span>
